9dd11fbd92fa745e0a6ed2edd44bc5f912b681e2
Gary Russell <garyrussell@Garys-MBP.home>
2021-01-21 19:00:52 -0800

    added missing files

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+import Amplify, {Auth} from 'aws-amplify'
+import config from '../aws-exports'
+Amplify.configure(config);
+Auth.configure({...config, authenticationFlowType: 'USER_PASSWORD_AUTH'});
+
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site with multi-user authentication powered by <a href="https://amplify.aws">AWS Amplify</a></p>
+    <p>Create a new account: <Link to="/app/signup">Sign Up</Link></p>
+    <Link to="/app/login">Sign In</Link><br />
+    <Link to="/app/home">Home</Link><br />
+    <Link to="/app/profile">Your profile</Link>
+  </Layout>
+)
+
+export default IndexPage
